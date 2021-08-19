9bc1b1135325c2a1cbcb5832fed318e129efa6e0
roger chat <mattrogerpelosi@gmail.com>
2021-08-19 16:53:26 -0400

    commit tree 9f332c65434d1efc714d84dc367e7a172d2b1862

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Add your code here
+function submitData(nameData, emailData) {
+      return fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: nameData,
+            email: emailData
+        })
+      })
+      .then(resp => resp.json())
+      .then(respObj => {
+          const body = document.querySelector('body');
+          const newEl = document.createElement('p');
+          newEl.textContent = respObj.id;
+          body.appendChild(newEl);
+      })
+      .catch(error => {
+          const body = document.querySelector('body');
+          const newErr = document.createElement('h1');
+          newErr.textContent = error.message;
+          body.appendChild(newErr);
+
+      })
+  } 
